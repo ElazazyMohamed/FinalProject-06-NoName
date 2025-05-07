a463eb512c6096eb7427ff3f308122566eefb6ae
Mohamed <mohamed.mohsen1701@gmail.com>
2025-05-08 00:14:42 +0300

    Implement ReminderFactory interface and its implementation for dynamic reminder creation

--- reminder/src/main/java/com/example/reminder/factory/ReminderFactory.java
+++ reminder/src/main/java/com/example/reminder/factory/ReminderFactory.java
@@ -1,4 +1,12 @@
 package com.example.reminder.factory;
 
-public class ReminderFactory {
+import com.example.reminder.model.Reminder;
+
+import java.time.LocalDateTime;
+import java.util.function.Supplier;
+
+public interface ReminderFactory {
+
+    void registerReminder(String type, Supplier<? extends Reminder> constructor);
+    Reminder createReminder(String type);
 }
--- reminder/src/main/java/com/example/reminder/factory/ReminderFactoryImpl.java
+++ reminder/src/main/java/com/example/reminder/factory/ReminderFactoryImpl.java
@@ -1,4 +1,26 @@
 package com.example.reminder.factory;
 
-public class ReminderFactoryImpl {
+import com.example.reminder.model.Reminder;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.function.Supplier;
+
+public class ReminderFactoryImpl implements ReminderFactory {
+
+    private final Map<String, Supplier<? extends Reminder>> registry = new HashMap<>();
+
+    @Override
+    public void registerReminder(String type, Supplier<? extends Reminder> constructor) {
+        registry.put(type, constructor);
+    }
+
+    @Override
+    public Reminder createReminder(String type) {
+        Supplier<? extends Reminder> constructor = registry.get(type);
+        if (constructor == null) {
+            throw new IllegalArgumentException("Unknown reminder type: " + type);
+        }
+        return constructor.get();
+    }
 }
