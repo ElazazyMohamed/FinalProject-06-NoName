009d5c00b69c68fe328a3f6e77b3ccfac1b3a514
Youssef-Haddad <youssefhaddad1@hotmail.com>
2025-05-11 12:31:57 +0300

    Update NotificationService.java

--- notification/src/main/java/com/example/notification/service/NotificationService.java
+++ notification/src/main/java/com/example/notification/service/NotificationService.java
@@ -1,4 +1,25 @@
 package com.example.notification.service;
 
+import com.example.notification.dto.UserResponse;
+import com.example.notification.model.ReminderEvent;
+import com.example.notification.service.strategy.NotificationStrategy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
 public class NotificationService {
+    private final Map<String, NotificationStrategy> strategies; // Auto-injected strategies
+
+    public void sendNotification(ReminderEvent event, UserResponse user) {
+        String strategyType = event.getNotificationType().toLowerCase() + "Strategy";
+        NotificationStrategy strategy = strategies.get(strategyType);
+        if (strategy != null) {
+            strategy.send(event, user);
+        } else {
+            throw new IllegalArgumentException("Invalid strategy: " + strategyType);
+        }
+    }
 }
