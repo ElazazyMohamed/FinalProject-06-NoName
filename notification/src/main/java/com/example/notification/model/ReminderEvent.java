f5f49f3cb6e732de5302dded9730b69857b0e4be
mohamedyosry55895 <101947515+mohamedyosry55895@users.noreply.github.com>
2025-05-11 18:22:49 +0300

    Update ReminderEvent.java

--- notification/src/main/java/com/example/notification/model/ReminderEvent.java
+++ notification/src/main/java/com/example/notification/model/ReminderEvent.java
@@ -1,4 +1,19 @@
 package com.example.notification.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class ReminderEvent {
+    private UUID reminderId;
+    private Integer userId;
+    private UUID noteId;
+    private LocalDateTime time;
+    private String notificationType; // e.g., "EMAIL", "SMS", "IN_APP"
 }
